6de58e5ad95b68b1bc848f089f155e06acd15103
Christopher Tønnesland <christopher.tonnesland@gmail.com>
2022-06-27 21:41:14 +0200

    .

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -5,7 +5,10 @@ function About() {
   <div className="content-container about">
      <div className="container">
     <h1>About</h1>
-
+      <article>
+         <p>This is a about page</p>
+         <p>It will come content here when the strapi backend is up and running.</p>
+      </article>
    </div>
   </div>
  );
